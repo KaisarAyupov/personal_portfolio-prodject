f6d60a1111f1049387bc38f1fe92ccecfbdb943f
caesar699 <caesar699@gmail.com>
2021-02-03 16:55:48 +0600

    front_20010103_ed01

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+import readtime
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
 
+    def get_readtime(self):
+        result = readtime.of_text(self.description)
+        return result.text
+
     def __str__(self):
         return self.title
-
